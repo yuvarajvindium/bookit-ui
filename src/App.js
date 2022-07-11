e73c359c018b0392ffba15cc0856f0df4bc98995
Yuvaraj V <yuvaraj.v@indiumsoft.com>
2022-07-11 06:11:29 +0530

    changed routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home.lazy";
 import About from "./pages/About/About.lazy";
+import Signin from "./pages/Signin/Signin.lazy";
+import Signup from "./pages/Signup/Signup.lazy";
+import Forgotpassword from "./pages/Forgotpassword/Forgotpassword.lazy";
+import Resetpassword from "./pages/Resetpassword/Resetpassword.lazy";
 import BookSpaceModal from "./pages/BookSpace/BookSpaceModal";
+import MyProfile from "./pages/MyProfile/MyProfile.lazy";
+import MyBookings from "./pages/MyBookings/MyBookings.lazy";
 import { BookSpaceConfirmation } from "./pages/BookSpace/BookSpaceConfirmation";
 
 function App() {
@@ -13,12 +19,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/signin" element={<About />} />
-        <Route path="/signup" element={<About />} />
-        <Route path="/forgot-password" element={<About />} />
-        <Route path="/reset-password" element={<About />} />
-        <Route path="/me" element={<About />} />
-        <Route path="/bookings" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<Forgotpassword />} />
+        <Route path="/reset-password" element={<Resetpassword />} />
+        <Route path="/me" element={<MyProfile />} />
+        <Route path="/bookings" element={<MyBookings />} />
         <Route path="/book-space" element={<BookSpaceModal />} />
         <Route
           path="/book-space-confirmation"
