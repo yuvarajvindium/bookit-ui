a9ec9091a3c02d02554780dd4c294a1a38a7e0ee
Yuvaraj V <yuvaraj.v@indiumsoft.com>
2022-07-11 06:14:25 +0530

    meta data cleanup & added scss files for bootstrap

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/scss/bootstrap.scss";
+import "bootstrap-icons/font/bootstrap-icons.scss";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
