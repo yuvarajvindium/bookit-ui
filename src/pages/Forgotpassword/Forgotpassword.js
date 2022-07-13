5f315e851cbf571e59b2297df7f6438666d7aa79
pemadechen209 <padenlaa@gmail.com>
2022-07-13 15:07:39 +0530

    updated design for signup and forgot password

--- src/pages/Forgotpassword/Forgotpassword.js
+++ src/pages/Forgotpassword/Forgotpassword.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Forgotpassword.scss';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from "react-bootstrap";
 
@@ -35,23 +34,26 @@ const Forgotpassword = () => {
 
   return <div className="Form container-fluid">
     <section className="row form-row justify-content-center ">
-      <section className="col-12 col-sm-4 col-md-4 form-main-page">
+      <section className="col-12 col-sm-12 col-md-5 form-main-page">
 
         <div className="mt-3">
           <h1 className="text-center form-header mt-5"> Bookit</h1>
-          <p className="text-center p-color ">{paraText}</p>
+          <p className="text-center p-area mt-2 ">{paraText}</p>
           <form className="form-container" onSubmit={handleSubmit}>
             <input type="text" className="form-control input-form" name="email" placeholder="Email"
               onChange={handleChange} />
             {errors.email && <span className="text-danger">{errors.email}</span>}
             <section className="d-grid gap-2">
-              <Button type="submit" className="btn btn-color forgot-button mt-3" disabled={Object.values(errors).some(v => v)}>Send Link</Button>
+              <Button type="submit" className="btn forgot-button mt-3"
+                disabled={Object.values(errors).some(v => v)}
+              >Send Link</Button>
             </section>
-            <div className="row">
+
+            <section className="row">
               <div className="col-5 line"></div>
               <div className="col-2 text-center"> OR </div>
               <div className="col-5 line"></div>
-            </div>
+            </section>
             <Link to='/signin' className="link-area"><p className="mt-3 back-login-p" >Back to Login  </p></Link>
           </form>
         </div>
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import './Signup.scss';
 import validateForm from '../../services/Validation';
-import { useState } from 'react';
 import { Button } from "react-bootstrap";
 
 const Signup = () => {
@@ -12,37 +11,37 @@ const Signup = () => {
   const handleChange = ({ target: { name, value } }) => {
     let updateValues = { ...values, [name]: value }
     setValues(updateValues);
-    setErrors(validateForm(updateValues));
   }
 
   const handleSubmit = (e) => {
-    console.log(errors);
     e.preventDefault();
+    setErrors(validateForm(values));
+    if (!Object.values(validateForm(values)).some(v => v)) {
+      console.log('signed Up')
+    }
 
   }
 
   return <div className="Form container-fluid">
     <section className="row form-row justify-content-center ">
-      <section className="col-12 col-sm-4 col-md-4 form-main-page">
-
-        <h1 className="text-center form-header mt-5"> Bookit</h1>
-        <form className="form-container mt-2" onSubmit={handleSubmit}>
+      <section className="col-12 col-sm-6 col-md-5 form-main-page">
+        <h1 className="text-center form-header"> Bookit</h1>
+        <form className="form-container " onSubmit={handleSubmit}>
           <h3 className="signup-text">Sign up</h3>
-          <label className="mt-3 label-text">Name</label>
+          <label className="label-text mt-3">Name</label>
           <input type="text" className="form-control input-form" name="name" onChange={handleChange} />
-          {errors.name && <span className="text-danger">{errors.name}</span>}<br />
+          {errors.name && <span className="mt-1 mb-2 text-danger">{errors.name}</span>}<br />
           <label className="label-text">Email</label>
           <input type="text" className="form-control input-form" name="email" onChange={handleChange} />
-          {errors.email && <span className="text-danger">{errors.email}</span>}<br />
+          {errors.email && <span className="mt-1 mb-2 text-danger">{errors.email}</span>}<br />
           <label className="label-text" >Password</label>
           <input type="password" className="form-control input-form" name="password" onChange={handleChange} />
-          {errors.password && <span className="text-danger">{errors.password}</span>}<br />
+          {errors.password && <span className=" mt-1 mb-2 text-danger">{errors.password}</span>}<br />
           <section className="d-grid gap-2">
-            <Button type="submit" className="btn btn-color"
-              disabled={Object.values(errors).some(v => v)}>Sign up</Button>
+            <Button type="submit" className="btn btn-color">Sign up</Button>
           </section>
           <section>
-            <p className="text-center mt-3 p-color">By signing up you agree to our <br />Terms & Condition</p>
+            <p className="text-center mt-3 p-area">By signing up you agree to our Terms & Condition</p>
           </section>
         </form>  </section>
     </section>
