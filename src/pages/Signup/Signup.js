28812ba2977964f99feb31ea81f08e4c2796dfe7
pemadechen209 <padenlaa@gmail.com>
2022-07-15 15:07:53 +0530

    changed navigation

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -37,7 +37,7 @@ const Signup = () => {
         setColor("success");
         setHeader("Success");
         setContent("User registered successfully");
-        navigate('/signin');
+        navigate('/');
       }
 
     }
